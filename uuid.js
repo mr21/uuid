6f1aea576cb9345e381b871cb69eda1f55ec4879
Thomas Tortorini <thomastortorini@gmail.com>
2019-05-09 01:03:52 +0200

    Update uuid.js

--- uuid.js
+++ uuid.js
@@ -2,16 +2,16 @@
 
 function newUUID() {
 	const rnd = crypto.getRandomValues( new Uint8Array( 36 ) ),
-		uuid = rnd.reduce( ( arr, n ) => {
+		s = rnd.reduce( ( arr, n ) => {
 			arr.push( ( n % 16 ).toString( 16 ) );
 			return arr;
 		}, [] );
 
-	uuid[ 14 ] = "4";
-	uuid[ 19 ] = ( 8 + rnd[ 19 ] % 4 ).toString( 16 );
-	uuid[ 8 ] =
-	uuid[ 13 ] =
-	uuid[ 18 ] =
-	uuid[ 23 ] = "-";
-	return uuid.join( "" );
+	s[ 14 ] = "4";
+	s[ 19 ] = ( 8 + rnd[ 19 ] % 4 ).toString( 16 );
+	s[ 8 ] =
+	s[ 13 ] =
+	s[ 18 ] =
+	s[ 23 ] = "-";
+	return s.join( "" );
 }
